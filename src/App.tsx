fc9a0c28776f6a2148c01e06af2b7bda82526ded
Alena Fatianova <ravemybody@gmail.com>
2023-12-14 00:13:28 +0100

    Create dark mode feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import useLocalStorage from 'use-local-storage'
 import './App.css';
 
-function App() {
+export const App: React.FC = () =>  {
+  
+  const defaultDark = window.matchMedia('(prefers-color-scheme:dark)').matches
+  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light')
+  
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light'
+    setTheme(newTheme)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper' data-theme={theme}>
+      <button onClick={switchTheme}>Switch theme</button>
+      <p>hello</p>
+      <main className='content'></main>
     </div>
   );
 }
 
-export default App;
