7d54f65623d01e31471ec515fc7765e8083902b7
Alena Fatianova <ravemybody@gmail.com>
2023-12-27 00:14:39 +0100

    Create Invoices page

--- src/pages/invoices/Invoices.tsx
+++ src/pages/invoices/Invoices.tsx
@@ -1,7 +1,66 @@
-import React from 'react'
+import { Table, TableProps } from "antd";
+import React from "react";
+import { Header } from "../../components/header/Header";
+import "./Invoices.css";
+import { mockDataInvoices } from "../../data/mockDataInvoices";
 
-export const Invoices = () => {
+type DataType = {
+  key?: string;
+  id?: any;
+  title?: string;
+  dataIndex: string
+  className?: string
+};
+
+const columns: DataType[] = [
+  {
+    id: 1,
+    title: "Name",
+    key: "Name",
+    dataIndex: "name"
+  },
+  {
+    id: 2,
+    title: "Email",
+    key: "Email",
+    dataIndex: "email"
+  },
+  {
+    id: 3,
+    title: "Phone",
+    key: "Phone",
+    dataIndex: "phone"
+  },
+  {
+    id: 4,
+    title: "Cost",
+    key: "Cost",
+    dataIndex: "cost",
+  },
+  {
+    id: 5,
+    title: "Date",
+    key: "Date",
+    dataIndex: "date",
+  }
+]
+
+export const Invoices: React.FC<TableProps<DataType>> = () => {
+
+
   return (
-    <div>Invoices</div>
-  )
-}
+    <div className="invoices-wrapper">
+      <Header title="INVOICES" subtitle="Invoices Balance" />
+      <div className="invoices-table-wrapper">
+        <Table
+          dataSource={mockDataInvoices}
+          columns={columns}
+          rowKey={"id"}
+          className={"invoices-table"}
+          pagination={{ position: ["bottomRight"] }}
+        /> 
+      </div>
+    </div>
+  );
+};
+
